5066e83a5444408bf207d23b24406c4a44f44b60
scottfulton2 <43143379+scottfulton2@users.noreply.github.com>
2022-02-04 17:47:29 +0000

    Foreign Keys Added + Refactoring
    Added Foreign Keys to REQUESTS table, tidied up code slightly (Refactoring) and initial creation of Materials Table

--- Database Code/CreateDatabase.sql
+++ Database Code/CreateDatabase.sql
@@ -10,7 +10,9 @@ CREATE TABLE `REQUESTS` (
 	`RequestID` int(11) NOT NULL,
     `EquipmentID` int(11) NOT NULL,
     `MaterialID` int(11) NOT NULL,
-    PRIMARY KEY (RequestID)
+    PRIMARY KEY (RequestID),
+    FOREIGN KEY (EquipmentID) REFERENCES EQUIPMENT(EquipmentID),
+	FOREIGN KEY (MaterialID) REFERENCES MATERIALS(MaterialID)
 );
 
 
@@ -18,25 +20,28 @@ CREATE TABLE `REQUESTS` (
 DROP TABLE IF EXISTS `EQUIPMENT`;
 CREATE TABLE `EQUIPMENT` (
 	`EquipmentID` int(11) NOT NULL,
-    `EquipmentName` varchar(10) DEFAULT NULL,
-    `EquipmentDescr`varchar(200) DEFAULT NULL,
-    `Location`varchar(20) DEFAULT NULL,
+    `EquipmentName` varchar(20) DEFAULT NULL,
+    `Descr` varchar(200) DEFAULT NULL,
+    `Location` varchar(20) DEFAULT NULL,
     `Cost` Decimal(6,2) DEFAULT NULL,
     `DateAdded` date,
     PRIMARY KEY (EquipmentID)
 );
 
 
-/*
-DROP TABLE IF EXISTS `REQUESTS`;
 
-CREATE TABLE `REQUESTS` (
-	`RequestID` int(11) NOT NULL,
-    
-PRIMARY KEY (RequestID)
+DROP TABLE IF EXISTS `MATERIALS`;
+CREATE TABLE `MATERIALS` (
+	`MaterialID` int(11) NOT NULL,
+    `MaterialName` varchar(20) DEFAULT NULL,
+    `Cost` Decimal(6,2) DEFAULT NULL,
+    `UnitOfMeasure` varchar(10) DEFAULT NULL,
+    `Descr` varchar(200) DEFAULT NULL,
+    `DateAdded` date,
+	PRIMARY KEY (MaterialID)
 )
 
-
+/*
 DROP TABLE IF EXISTS `REQUESTS`;
 
 CREATE TABLE `REQUESTS` (
