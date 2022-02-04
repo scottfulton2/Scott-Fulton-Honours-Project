1435a446d74dd82a54490c0bfa3bf323bd1b7b0e
scottfulton2 <43143379+scottfulton2@users.noreply.github.com>
2022-02-04 16:59:13 +0000

    Equipment Table
    The Equipment Table has been initially setup

--- Database Code/CreateDatabase.sql
+++ Database Code/CreateDatabase.sql
@@ -4,21 +4,26 @@ CREATE DATABASE `scottfultondb`;
 USE `scottfultondb`;
 
 
-DROP TABLE IF EXISTS `REQUESTS`;
 
+DROP TABLE IF EXISTS `REQUESTS`;
 CREATE TABLE `REQUESTS` (
 	`RequestID` int(11) NOT NULL,
     PRIMARY KEY (RequestID)
 );
 
 
-DROP TABLE IF EXISTS `EQUIPMENT`;
 
+DROP TABLE IF EXISTS `EQUIPMENT`;
 CREATE TABLE `EQUIPMENT` (
 	`EquipmentID` int(11) NOT NULL,
     `EquipmentName` varchar(10) DEFAULT NULL,
+    `EquipmentDescr`varchar(200) DEFAULT NULL,
+    `Location`varchar(20) DEFAULT NULL,
+    `Cost` Decimal(6,2) DEFAULT NULL,
+    `DateAdded` date,
     PRIMARY KEY (EquipmentID)
 );
+
 
 
 DROP TABLE IF EXISTS `REQUESTS`;
